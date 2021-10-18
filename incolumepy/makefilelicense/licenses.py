0cb2a2e435b80e3de0bd10da2fdf972ec72a98d3
Britodfbr <britodfbr@gmail.com>
2021-10-18 16:02:38 -0300

    Docstring atualizada

--- incolumepy/makefilelicense/licenses.py
+++ incolumepy/makefilelicense/licenses.py
@@ -13,26 +13,21 @@ import requests
 def licenses(license: str = '') -> bool:
     """
     Got license text
-    :param license: [agpl gpl lgpl mit mpl apache bsl unlicense]
-    :return: the license of reference
+    :param license: [agpl gpl lgpl mit mpl bsl apache unlicense] default=mit
+    :return: A file named LICENSE with the license of reference
     """
     license = license or 'MIT'
     url = 'https://choosealicense.com/licenses/'
 
     try:
         req = requests.get(url)
-        # print(req.content)
         soup = BeautifulSoup(req.content, 'html.parser')
         query = soup.find('a', href=re.compile(f'/licenses/{license}*', flags=re.I))
-        # print(query)
-        # print(query.get('href'))
 
         nurl = urljoin(url, query.get('href'))
-        # print(nurl)
         page = requests.get(nurl)
         soup = BeautifulSoup(page.content, 'html.parser')
         q = soup.find('pre', attrs={'id': 'license-text'})
-        # print(q.text)
         Path('LICENSE').write_text(q.text)
         return True
     except (AttributeError, requests.RequestException):
@@ -40,11 +35,5 @@ def licenses(license: str = '') -> bool:
 
 
 if __name__ == '__main__':
-    # licenses('AGPL')
-    # print('===')
-    # licenses()
-    # licenses('apache')
-    # licenses('mozilla')
-    # licenses('boost')
-    # licenses('unlicense')
-    print(licenses('gpl'))
+    licenses('AGPL')
+    licenses('gpl')
