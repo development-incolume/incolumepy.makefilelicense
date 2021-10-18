17d1fc6403873cc086ff45f5cdb828619eb83f5f
Britodfbr <britodfbr@gmail.com>
2021-10-18 10:25:29 -0300

    Versão do projeto e versão do pacote homogenizadas

--- tests/test_incolumepy_makefilelicense.py
+++ tests/test_incolumepy_makefilelicense.py
@@ -1,5 +1,6 @@
 from incolumepy.makefilelicense import __version__
+import re
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert re.fullmatch(r'\d(.\d){2}(-\w+.\d+)?', __version__, flags=re.I)
