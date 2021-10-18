b7f0c4a1fdf4c17be0aaa7cba6b7f7ef165d2356
Britodfbr <britodfbr@gmail.com>
2021-10-18 15:47:50 -0300

    Testes e conformações para módulo licenses

--- incolumepy/makefilelicense/licenses.py
+++ incolumepy/makefilelicense/licenses.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 __author__ = "@britodfbr"  # pragma: no cover
 
+import bs4
 from bs4 import BeautifulSoup
 from pathlib import Path
 from urllib.parse import urljoin
@@ -9,30 +10,33 @@ import re
 import requests
 
 
-def licenses(license: str = '') -> str:
+def licenses(license: str = '') -> bool:
     """
     Got license text
-    :param license: [agpl gpl lgpl mit mozilla apache boost unlicense]
+    :param license: [agpl gpl lgpl mit mpl apache bsl unlicense]
     :return: the license of reference
     """
     license = license or 'MIT'
     url = 'https://choosealicense.com/licenses/'
 
-    req = requests.get(url)
-    # print(req.content)
-    soup = BeautifulSoup(req.content, 'html.parser')
-    query = soup.find('a', string=re.compile(license, flags=re.I))
-    # print(query)
-    # print(query.get('href'))
+    try:
+        req = requests.get(url)
+        # print(req.content)
+        soup = BeautifulSoup(req.content, 'html.parser')
+        query = soup.find('a', href=re.compile(f'/licenses/{license}*', flags=re.I))
+        # print(query)
+        # print(query.get('href'))
 
-    nurl = urljoin(url, query.get('href'))
-    # print(nurl)
-    page = requests.get(nurl)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    q = soup.find('pre', attrs={'id': 'license-text'})
-    # print(q.text)
-    Path('LICENSE').write_text(q.text)
-    return q.text
+        nurl = urljoin(url, query.get('href'))
+        # print(nurl)
+        page = requests.get(nurl)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        q = soup.find('pre', attrs={'id': 'license-text'})
+        # print(q.text)
+        Path('LICENSE').write_text(q.text)
+        return True
+    except (AttributeError, requests.RequestException):
+        return False
 
 
 if __name__ == '__main__':
--- tests/test_incolumepy_makefilelicense.py
+++ tests/test_incolumepy_makefilelicense.py
@@ -1,6 +1,30 @@
 from incolumepy.makefilelicense import __version__
+from incolumepy.makefilelicense.licenses import licenses
 import re
+from pathlib import Path
+import pytest
 
 
 def test_version():
     assert re.fullmatch(r'\d(.\d){2}(-\w+.\d+)?', __version__, flags=re.I)
+
+
+@pytest.mark.parametrize(
+    'license, title',
+    [
+        ('gpl', r'GNU GENERAL PUBLIC LICENSE'),
+        ('agpl', r'GNU AFFERO GENERAL PUBLIC LICENSE'),
+        ('lgpl', r'GNU LESSER GENERAL PUBLIC LICENSE'),
+        ('mit', r'MIT License'),
+        ('mpl', r'Mozilla Public License Version 2.0'),
+        ('bsl', r'Boost Software License - Version 1.0 - August 17th, 2003'),
+        ('apache', r'Apache License'),
+        ('unlicense', r'This is free and unencumbered software released into the public domain.'),
+    ]
+)
+def test_licenses(license, title):
+    assert licenses(license)
+    file = Path('LICENSE')
+    with open('LICENSE') as f:
+        assert re.match(title, f.readline().strip())
+    file.unlink(missing_ok=True)
