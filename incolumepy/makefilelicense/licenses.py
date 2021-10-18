c6e1724497117be3e60ad43d7b29dde8b6933ebb
Britodfbr <britodfbr@gmail.com>
2021-10-18 14:50:22 -0300

    Convertido script para módulo

--- incolumepy/makefilelicense/licenses.py
+++ incolumepy/makefilelicense/licenses.py
@@ -1,25 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 __author__ = "@britodfbr"  # pragma: no cover
-import requests
+
 from bs4 import BeautifulSoup
-import re
-from urllib.parse import urljoin
 from pathlib import Path
+from urllib.parse import urljoin
+import re
+import requests
 
-url = 'https://choosealicense.com/licenses/'
 
-req = requests.get(url)
-# print(req.content)
-soup = BeautifulSoup(req.content, 'html.parser')
-query = soup.find('a', string=re.compile('AGPL'))
-# print(query)
-# print(query.get('href'))
+def licenses(license: str = '') -> str:
+    license = license or 'MIT'
+    url = 'https://choosealicense.com/licenses/'
 
-nurl = urljoin(url, query.get('href'))
-# print(nurl)
-page = requests.get(nurl)
-soup = BeautifulSoup(page.content, 'html.parser')
-q = soup.find('pre', attrs={'id': 'license-text'})
-print(q.text)
-Path('LICENSE').write_text(q.text)
+    req = requests.get(url)
+    # print(req.content)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    query = soup.find('a', string=re.compile(license, flags=re.I))
+    # print(query)
+    # print(query.get('href'))
+
+    nurl = urljoin(url, query.get('href'))
+    # print(nurl)
+    page = requests.get(nurl)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    q = soup.find('pre', attrs={'id': 'license-text'})
+    print(q.text)
+    Path('LICENSE').write_text(q.text)
+
+
+if __name__ == '__main__':
+    licenses('AGPL')
+    print('===')
+    licenses()
