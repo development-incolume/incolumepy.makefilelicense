b6cbad6027ab786dae35aac2f1ef60f4cf61a726
Britodfbr <britodfbr@gmail.com>
2021-10-20 20:21:28 -0300

    Tratativa na correção de erro na versão do pacote wheels em 2021-10-20 20:21:28

--- incolumepy/makefilelicense/__init__.py
+++ incolumepy/makefilelicense/__init__.py
@@ -1,17 +1,26 @@
 import toml
 from pathlib import Path
 
-__root__ = Path(__file__).parent.parent.parent
+# __root__ = Path(__file__).parent.parent.parent
 # assert __root__.is_file(), f'{__root__}'
 
 
-def update_version():
-    with Path('version.txt').open('w') as f:
-        f.write(
-            f"{toml.load(__root__.joinpath('pyproject.toml'))['tool']['poetry']['version']}\n"
-        )
+# def update_version():
+#     with Path('version.txt').open('w') as f:
+#         f.write(
+#             f"{toml.load(__root__.joinpath('pyproject.toml'))['tool']['poetry']['version']}\n"
+#         )
+#
+#
+# update_version()
+# with open('version.txt') as f:
+#     __version__ = f.readline().strip()
+path = Path(__file__)
 
 
-update_version()
-with open('version.txt') as f:
-    __version__ = f.readline().strip()
+if __name__ == '__main__':
+    print(list(path.parents))
+    print(path.parents[2])
+    print(type(path.parents[2]))
+    print(path.parents[2].joinpath('incolumepy', 'makefilelicense'))
+    assert path.is_file(), f"{path}"
