90dd198009aa06514f47dfa9e09ef5bc5702111b
Britodfbr <britodfbr@gmail.com>
2021-10-21 19:32:24 -0300

    Trativa refactor offline

--- incolumepy/makefilelicense/licenses.py
+++ incolumepy/makefilelicense/licenses.py
@@ -3,11 +3,8 @@
 __author__ = "@britodfbr"  # pragma: no cover
 
 from functools import partial
-from bs4 import BeautifulSoup
 from pathlib import Path
-from urllib.parse import urljoin
 import re
-import requests
 
 
 def licenses(license: str = '') -> bool:
@@ -16,21 +13,17 @@ def licenses(license: str = '') -> bool:
     :param license: [agpl gpl lgpl mit mpl bsl apache unlicense] default=mit
     :return: A file named LICENSE with the license of reference
     """
-    license = license or 'MIT'
-    url = 'https://choosealicense.com/licenses/'
+    license = license.casefold() or 'mit'
+    repo = Path('licenses')
 
     try:
-        req = requests.get(url)
-        soup = BeautifulSoup(req.content, 'html.parser')
-        query = soup.find('a', href=re.compile(f'/licenses/{license}*', flags=re.I))
-
-        nurl = urljoin(url, query.get('href'))
-        page = requests.get(nurl)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        q = soup.find('pre', attrs={'id': 'license-text'})
-        Path('LICENSE').write_text(q.text)
+        # license_file = [x for x in repo.glob('*.txt')]
+        # license_file = [str(x) for x in repo.glob('*.txt')]
+        license_file, = [x for x in repo.glob('*.txt') if re.match(f'.*/{license}.txt', str(x), flags=re.I)]
+        Path('LICENSE').write_text(license_file.read_text())
+        print(license_file)
         return True
-    except (AttributeError, requests.RequestException):
+    except (AttributeError, ):
         return False
 
 
