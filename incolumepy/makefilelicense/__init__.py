42ed7373bad9ef9d799c97b3616d6438678d66d4
Britodfbr <britodfbr@gmail.com>
2021-10-20 21:00:42 -0300

    Nova forma para carregar valor em __version__

--- incolumepy/makefilelicense/__init__.py
+++ incolumepy/makefilelicense/__init__.py
@@ -1,3 +1,12 @@
 import toml
 from pathlib import Path
-__version__ = "0.1.4-alpha.1"
+
+
+with Path(__file__).parents[0].joinpath('version.txt').open('w') as f:
+    f.write(toml.load(Path(__file__).parents[2].joinpath('pyproject.toml'))['tool']['poetry']['version'])
+
+with Path(__file__).parents[0].joinpath('version.txt').open() as f:
+    __version__ = f.readline().strip()
+
+if __name__ == '__main__':
+    ...
