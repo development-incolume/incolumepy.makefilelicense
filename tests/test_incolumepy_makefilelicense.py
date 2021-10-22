a485c99f8d8c9fb35d0be43e369938254063155c
Britodfbr <britodfbr@gmail.com>
2021-10-22 15:35:40 -0300

    Format black

--- tests/test_incolumepy_makefilelicense.py
+++ tests/test_incolumepy_makefilelicense.py
@@ -9,39 +9,45 @@ from tempfile import NamedTemporaryFile
 
 @pytest.fixture()
 def outputfile():
-    return NamedTemporaryFile(prefix='license-', suffix='.txt').name
+    return NamedTemporaryFile(prefix="license-", suffix=".txt").name
 
 
 def test_version():
-    assert re.fullmatch(r'\d(.\d){2}(-\w+.\d+)?', __version__, flags=re.I)
+    assert re.fullmatch(r"\d(.\d){2}(-\w+.\d+)?", __version__, flags=re.I)
 
 
 def test_file_version():
-    file = __root__.joinpath('version.txt')
+    file = __root__.joinpath("version.txt")
     assert file.is_file(), f"{file}"
 
 
 def test_file_version_content():
-    file = __root__.joinpath('version.txt')
+    file = __root__.joinpath("version.txt")
     assert __version__ == file.read_text().strip()
 
 
 @pytest.mark.parametrize(
-    'license, title',
+    "license, title",
     [
-        ('agpl', r'GNU AFFERO GENERAL PUBLIC LICENSE'),
-        ('apache', r'Apache License'),
-        ('bsl', r'Boost Software License - Version 1.0 - August 17th, 2003'),
-        ('CC0', 'Creative Commons Legal Code'),
-        ('cc0', 'Creative Commons Legal Code'),
-        ('GPL', r'GNU GENERAL PUBLIC LICENSE'),
-        ('gpl', r'GNU GENERAL PUBLIC LICENSE'),
-        ('lgpl', r'GNU LESSER GENERAL PUBLIC LICENSE'),
-        ('mit', r'MIT License'),
-        ('mpl', r'Mozilla Public License Version 2.0'),
-        ('UNLICENSE', r'This is free and unencumbered software released into the public domain.'),
-        ('unlicense', r'This is free and unencumbered software released into the public domain.'),
-    ]
+        ("agpl", r"GNU AFFERO GENERAL PUBLIC LICENSE"),
+        ("apache", r"Apache License"),
+        ("bsl", r"Boost Software License - Version 1.0 - August 17th, 2003"),
+        ("CC0", "Creative Commons Legal Code"),
+        ("cc0", "Creative Commons Legal Code"),
+        ("GPL", r"GNU GENERAL PUBLIC LICENSE"),
+        ("gpl", r"GNU GENERAL PUBLIC LICENSE"),
+        ("lgpl", r"GNU LESSER GENERAL PUBLIC LICENSE"),
+        ("mit", r"MIT License"),
+        ("mpl", r"Mozilla Public License Version 2.0"),
+        (
+            "UNLICENSE",
+            r"This is free and unencumbered software released into the public domain.",
+        ),
+        (
+            "unlicense",
+            r"This is free and unencumbered software released into the public domain.",
+        ),
+    ],
 )
 def test_licenses(license, title, outputfile):
     assert licenses(license, outputfile)
@@ -52,7 +58,7 @@ def test_licenses(license, title, outputfile):
     file.unlink(missing_ok=True)
 
 
-@pytest.mark.parametrize('license', ['qgpl', 'xpto'])
+@pytest.mark.parametrize("license", ["qgpl", "xpto"])
 def test_licenses_raises(outputfile, license):
     file = Path(outputfile)
     with pytest.raises(LicenseUnavailable):
