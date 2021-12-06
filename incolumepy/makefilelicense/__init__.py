3e18c119f558c2cd4d550178a2f625ad5946f8e5
Britodfbr <britodfbr@gmail.com>
2021-12-06 20:31:52 -0300

    Applied Code style Black format automaticly at 2021-12-06 20:31:52

--- incolumepy/makefilelicense/__init__.py
+++ incolumepy/makefilelicense/__init__.py
@@ -5,13 +5,7 @@ import toml
 __root__ = Path(__file__).parents[0]
 
 with __root__.joinpath("version.txt").open("w") as f:
-    f.write(
-        "{}\n".format(
-            toml.load(Path(__file__).parents[2].joinpath("pyproject.toml"))["tool"][
-                "poetry"
-            ]["version"]
-        )
-    )
+    f.write("{}\n".format(toml.load(Path(__file__).parents[2].joinpath("pyproject.toml"))["tool"]["poetry"]["version"]))
 
 with __root__.joinpath("version.txt").open() as f:
     __version__ = f.readline().strip()
